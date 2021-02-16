346de7002cdc9632986008766648dd212b16f196
Adrian Ochmann <adrian.ochmann@outlook.com>
2021-02-16 14:36:14 +0100

    feat: added error constants

---
+++ Source/Cogworks.Umbraco.Essentials/Constants/ErrorConstants.cs
@@ -0,0 +1,8 @@
+﻿namespace Cogworks.Umbraco.Essentials.Constants
+{
+    public static class ErrorConstants
+    {
+        public const string PreventDuplicateRequestError = "PreventDuplicateRequest";
+        public const string PreventDuplicateRequestErrorMessage = "Looks like you accidentally tried to submit the form multiple times. Please try again.";
+    }
+}
