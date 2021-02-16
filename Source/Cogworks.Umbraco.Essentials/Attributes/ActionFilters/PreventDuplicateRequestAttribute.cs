d0e28bdc1f3ba6dd2b95b339b4c829b1a5ab5024
Adrian Ochmann <adrian.ochmann@outlook.com>
2021-02-16 14:36:38 +0100

    feat: added action filter attributes

---
+++ Source/Cogworks.Umbraco.Essentials/Attributes/ActionFilters/PreventDuplicateRequestAttribute.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Web;
+using System.Web.Mvc;
+using Cogworks.Umbraco.Essentials.Constants;
+
+namespace Cogworks.Umbraco.Essentials.Attributes.ActionFilters
+{
+    // Code from
+    // https://gist.github.com/jyarbro/facb175caed8eb9e2239faf750e37230
+    // https://stackoverflow.com/a/32807640
+    [AttributeUsage(AttributeTargets.Class | AttributeTargets.Method, AllowMultiple = false, Inherited = true)]
+    public class PreventDuplicateRequestAttribute : ActionFilterAttribute
+    {
+        public override void OnActionExecuting(ActionExecutingContext filterContext)
+        {
+            var currentToken = HttpContext.Current.Request["__RequestVerificationToken"];
+
+            if (currentToken == null)
+            {
+                return;
+            }
+
+            if (HttpContext.Current.Session["LastProcessedToken"] == null)
+            {
+                HttpContext.Current.Session["LastProcessedToken"] = currentToken;
+
+                return;
+            }
+
+            lock (HttpContext.Current.Session["LastProcessedToken"])
+            {
+                var lastToken = HttpContext.Current.Session["LastProcessedToken"].ToString();
+
+                if (lastToken == currentToken)
+                {
+                    filterContext.Controller.ViewData.ModelState.AddModelError(
+                        ErrorConstants.PreventDuplicateRequestError,
+                        ErrorConstants.PreventDuplicateRequestErrorMessage);
+
+                    return;
+                }
+
+                HttpContext.Current.Session["LastProcessedToken"] = currentToken;
+            }
+        }
+    }
+}
