aacd07a82be92c1b0490065922c3300485e1c9b9
Adrian Ochmann <adrian.ochmann@outlook.com>
2021-02-19 14:51:19 +0100

    feat: added string constants

---
+++ Source/Cogworks.Umbraco.Essentials/Constants/StringConstants.SeparatosConstants.cs
@@ -0,0 +1,11 @@
+﻿namespace Cogworks.Umbraco.Essentials.Constants
+{
+    public static partial class StringConstants
+    {
+        public static class Separators
+        {
+            public const string Comma = ",";
+            public const string Space = " ";
+        }
+    }
+}
