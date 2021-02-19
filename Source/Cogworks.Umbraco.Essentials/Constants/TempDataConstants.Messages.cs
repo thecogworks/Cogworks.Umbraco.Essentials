cc87dd0b672df03d165502ac463896fc594c633e
Adrian Ochmann <adrian.ochmann@outlook.com>
2021-02-19 14:50:06 +0100

    feat: added redirect actions

---
+++ Source/Cogworks.Umbraco.Essentials/Constants/TempDataConstants.Messages.cs
@@ -0,0 +1,12 @@
+﻿namespace Cogworks.Umbraco.Essentials.Constants
+{
+    public static partial class TempDataConstants
+    {
+        public static class MessageConstants
+        {
+            public const string Header = "Message.Info.Header";
+
+            public const string Message = "Message.Info.Message";
+        }
+    }
+}
