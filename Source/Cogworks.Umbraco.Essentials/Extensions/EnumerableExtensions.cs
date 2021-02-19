47152f8184834c7c8df17b15c717f60faf381f76
Adrian Ochmann <adrian.ochmann@outlook.com>
2021-02-19 15:22:20 +0100

    chore: removed extension that is in universal essentials

--- Source/Cogworks.Umbraco.Essentials/Extensions/EnumerableExtensions.cs
+++ Source/Cogworks.Umbraco.Essentials/Extensions/EnumerableExtensions.cs
@@ -1,8 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Linq;
-using Cogworks.Umbraco.Essentials.Constants;
-using Umbraco.Core.Models.PublishedContent;
 
 namespace Cogworks.Umbraco.Essentials.Extensions
 {
@@ -10,14 +7,5 @@ namespace Cogworks.Umbraco.Essentials.Extensions
     {
         public static bool HasAny<T>(this IEnumerable<T> items)
             => items != null && items.Any();
-
-        public static T FirstOrDefaultOfType<T>(this IEnumerable<IPublishedContent> publishedContents)
-            => publishedContents.OfType<T>().FirstOrDefault();
-
-        public static string JoinIfNotNull<TInput, TResult>(this IEnumerable<TInput> items, Func<TInput, TResult> func,
-            string separator = StringConstants.Separators.Space)
-            => items.HasAny()
-                ? string.Join(separator, items.Select(func))
-                : string.Empty;
     }
 }
