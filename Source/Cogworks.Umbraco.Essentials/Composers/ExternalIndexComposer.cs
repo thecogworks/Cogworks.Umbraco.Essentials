5f063dc0f7a06cd29c442238ec4ce1af9b6d6ade
Adrian Ochmann <adrian.ochmann@outlook.com>
2021-02-23 11:30:23 +0100

    feat: added external index composer

---
+++ Source/Cogworks.Umbraco.Essentials/Composers/ExternalIndexComposer.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections.Generic;
+using Umbraco.Core;
+using Umbraco.Core.Composing;
+using Umbraco.Core.Services;
+using Umbraco.Examine;
+
+namespace Cogworks.Umbraco.Essentials.Composers
+{
+    public class ExternalIndexComposer : IUserComposer
+    {
+        public void Compose(Composition composition)
+        {
+            // replace the default IUmbracoIndexConfig definition
+            composition.RegisterUnique<IUmbracoIndexConfig, ExternalIndexConfig>();
+        }
+    }
+
+    public class ExternalIndexConfig : UmbracoIndexConfig, IUmbracoIndexConfig
+    {
+        private readonly IPublicAccessService _publicAccessService;
+
+        public ExternalIndexConfig(IPublicAccessService publicAccessService) : base(publicAccessService)
+            => _publicAccessService = publicAccessService;
+
+        IContentValueSetValidator IUmbracoIndexConfig.GetPublishedContentValueSetValidator()
+            => new ContentValueSetValidator(true,
+                true,
+                _publicAccessService,
+                includeItemTypes: new List<string>()
+            );
+    }
+}
