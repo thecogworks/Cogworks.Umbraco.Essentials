48717b4618ae30161377dc188f51fee184d6d4ca
Adrian Ochmann <adrian.ochmann@outlook.com>
2021-02-23 08:55:09 +0100

    feat: added public access service

---
+++ Source/Cogworks.Umbraco.Essentials/Services/Interfaces/IPublicAccessService.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections.Generic;
+
+namespace Cogworks.Umbraco.Essentials.Services.Interfaces
+{
+    public interface IPublicAccessService
+    {
+        IEnumerable<int> GetContentIdsWithRestrictedAccess();
+    }
+}
---
+++ Source/Cogworks.Umbraco.Essentials/Services/PublicAccessService.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Cogworks.Umbraco.Essentials.Services.Interfaces;
+using Umbraco.Core.Scoping;
+
+namespace Cogworks.Umbraco.Essentials.Services
+{
+    public class PublicAccessService : IPublicAccessService
+    {
+        private readonly IScopeProvider _scopeProvider;
+
+        public PublicAccessService(IScopeProvider scopeProvider)
+            => _scopeProvider = scopeProvider
+                                ?? throw new ArgumentNullException(nameof(scopeProvider));
+
+        public IEnumerable<int> GetContentIdsWithRestrictedAccess()
+        {
+            using (var scope = _scopeProvider.CreateScope(autoComplete: true))
+            {
+                return scope.Database
+                    .Query<int>("SELECT nodeId FROM umbracoAccess")
+                    .ToList();
+            }
+        }
+    }
+}
