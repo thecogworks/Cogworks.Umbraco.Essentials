3b01731b3b7f872d5e909f6be12c242aae954521
Adrian Ochmann <adrian.ochmann@outlook.com>
2021-02-23 09:33:21 +0100

    feat: added robots handler

---
+++ Source/Cogworks.Umbraco.Essentials/Constants/RobotsConstants.cs
@@ -0,0 +1,8 @@
+﻿namespace Cogworks.Umbraco.Essentials.Constants
+{
+    public static class RobotsConstants
+    {
+        public const string Live = "Live";
+        public const string Disallowed = "Disallowed";
+    }
+}
---
+++ Source/Cogworks.Umbraco.Essentials/Handlers/RobotsHandler.cs
@@ -0,0 +1,35 @@
+﻿using System.IO;
+using System.Web;
+using Cogworks.Umbraco.Essentials.Configurations;
+using Cogworks.Umbraco.Essentials.Constants;
+
+namespace Cogworks.Umbraco.Essentials.Handlers
+{
+    public class RobotsHandler : IHttpHandler
+    {
+        public bool IsReusable => true;
+
+        public void ProcessRequest(HttpContext context)
+        {
+            var isProduction = IsProductionUrl(context);
+            var robotsTxtContent = isProduction
+                ? ReadFile(RobotsConstants.Live, context)
+                : ReadFile(RobotsConstants.Disallowed, context);
+
+            context.Response.ContentType = "text/plain";
+            context.Response.Clear();
+            context.Response.BufferOutput = true;
+            context.Response.Write(robotsTxtContent);
+        }
+
+        private static string ReadFile(string name, HttpContext context)
+            => File.ReadAllText(GetPath("robots." + name + ".txt", context));
+
+        private static string GetPath(string name, HttpContext context)
+            => context.Server.MapPath(VirtualPathUtility.ToAbsolute("~/" + name));
+
+        private static bool IsProductionUrl(HttpContext context)
+            => context.Request.Url?.Host == AppSettingsConfiguration.LiveDomain
+               || context.Request.UrlReferrer?.Host == AppSettingsConfiguration.LiveDomain;
+    }
+}
