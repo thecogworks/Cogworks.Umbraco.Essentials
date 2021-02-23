62aa375e1bddd3f1c6e223ad60f3a4860ad0c495
Adrian Ochmann <adrian.ochmann@outlook.com>
2021-02-23 10:01:04 +0100

    feat: added angular attribute

---
+++ Source/Cogworks.Umbraco.Essentials/Attributes/AngularAttributes/AngularJsonMediaFormatterCamelCase.cs
@@ -0,0 +1,11 @@
+﻿using Newtonsoft.Json.Serialization;
+using Umbraco.Web.WebApi;
+
+namespace Cogworks.Umbraco.Essentials.Attributes.AngularAttributes
+{
+    public class AngularJsonMediaFormatterCamelCase : AngularJsonMediaTypeFormatter
+    {
+        public AngularJsonMediaFormatterCamelCase()
+            => SerializerSettings.ContractResolver = new CamelCasePropertyNamesContractResolver();
+    }
+}
