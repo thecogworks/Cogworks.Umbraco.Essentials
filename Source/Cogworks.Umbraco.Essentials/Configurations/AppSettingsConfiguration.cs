f93e8ff796c99f39aeaa9a178eb9c19ff94b656c
Adrian Ochmann <adrian.ochmann@outlook.com>
2021-02-23 09:33:06 +0100

    feat: added app settings configuration

---
+++ Source/Cogworks.Umbraco.Essentials/Configurations/AppSettingsConfiguration.cs
@@ -0,0 +1,13 @@
+﻿using Cogworks.Umbraco.Essentials.Helpers;
+
+namespace Cogworks.Umbraco.Essentials.Configurations
+{
+    public static class AppSettingsConfiguration
+    {
+        public static string ApplicationHostName { get; } = AppSettings.Get<string>("ApplicationHostName");
+
+        public static string LiveDomain { get; } = AppSettings.Get<string>("LiveDomain");
+
+        public static bool IsMasterServer { get; } = AppSettings.Get<bool>("IsMasterServer");
+    }
+}
