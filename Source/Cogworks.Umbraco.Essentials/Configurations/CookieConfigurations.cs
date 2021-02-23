42782fd9a7ddd100747989eb97c663ffede55889
Adrian Ochmann <adrian.ochmann@outlook.com>
2021-02-23 09:55:42 +0100

    feat: added cookie helpers

---
+++ Source/Cogworks.Umbraco.Essentials/Configurations/CookieConfigurations.cs
@@ -0,0 +1,9 @@
+﻿using Cogworks.Umbraco.Essentials.Helpers;
+
+namespace Cogworks.Umbraco.Essentials.Configurations
+{
+    public static class CookieConfigurations
+    {
+        public static readonly int CookieDuration = AppSettings.Get<int>("Cookies.DurationInDays");
+    }
+}
