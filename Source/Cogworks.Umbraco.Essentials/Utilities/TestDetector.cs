45edb698635955507b7a57b98acc600749106d32
Adrian Ochmann <adrian.ochmann@outlook.com>
2021-02-23 09:25:50 +0100

    feat: added test detector

---
+++ Source/Cogworks.Umbraco.Essentials/Utilities/TestDetector.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Linq;
+
+namespace Cogworks.Umbraco.Essentials.Utilities
+{
+    public static class TestDetector
+    {
+        public static bool IsRunningFromXUnit => AppDomain.CurrentDomain
+            .GetAssemblies()
+            .Any(a => a.FullName.ToLowerInvariant().StartsWith("xunit.core"));
+    }
+}
