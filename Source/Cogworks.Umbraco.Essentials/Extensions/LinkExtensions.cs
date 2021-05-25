184400903c47ec6a3b995e84eb955517ea33d7b6
Alejandro Ocampo <alejandro.ocampo@wearecogworks.com>
2021-05-24 22:48:01 -0500

    feat: adding link HasValue extension

---
+++ Source/Cogworks.Umbraco.Essentials/Extensions/LinkExtensions.cs
@@ -0,0 +1,10 @@
+﻿using Cogworks.Essentials.Extensions;
+using Umbraco.Web.Models;
+
+namespace Cogworks.Umbraco.Essentials.Extensions
+{
+    public static class LinkExtensions
+    {
+        public static bool HasValue(this Link link) => link != null && link.Url.HasValue();
+    }
+}
