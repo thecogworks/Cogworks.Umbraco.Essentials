0dccbb16260bb2575aedba76caa2317fd06fb8e3
Alejandro Ocampo <alejandro.ocampo@wearecogworks.com>
2021-05-25 08:20:39 -0500

    chore: applying PR feedback

--- Source/Cogworks.Umbraco.Essentials/Controllers/Actions/ResponsiveImageSurfaceController.cs
+++ Source/Cogworks.Umbraco.Essentials/Controllers/Actions/ResponsiveImageSurfaceController.cs
@@ -15,9 +15,10 @@ namespace Cogworks.Umbraco.Essentials.Controllers.Actions
         public ResponsiveImageSurfaceController(IResponsiveImageBuilder responsiveImageBuilder) => _responsiveImageBuilder = responsiveImageBuilder;
 
         [ChildActionOnly]
-        public virtual ActionResult ResponsiveImage(IPublishedContent image, string cropPrefix, string altText = null, IReadOnlyDictionary<string, string> breakpoints = null,
-            string imageClass = null, string containerClass = null, string cropSeparator = StringConstants.Separators.Hyphen,
-            int? width = null, int? height = null, bool includeRetina = true, bool enableWebP = false, int? quality = null)
+        public virtual ActionResult ResponsiveImage(IPublishedContent image, string cropPrefix, string altText = null,
+            IReadOnlyDictionary<string, string> breakpoints = null, string imageClass = null, string containerClass = null,
+            string cropSeparator = StringConstants.Separators.Hyphen, int? width = null, int? height = null, bool includeRetina = true,
+            bool enableWebP = false, int? quality = null)
         {
             if (!image.HasValue())
             {
@@ -27,7 +28,7 @@ namespace Cogworks.Umbraco.Essentials.Controllers.Actions
             var model = _responsiveImageBuilder.Build(image, cropPrefix, altText, breakpoints,
                 imageClass, containerClass, cropSeparator, width, height, includeRetina, enableWebP, quality);
 
-            if (model == null)
+            if (!model.HasValue())
             {
                 return new EmptyResult();
             }
